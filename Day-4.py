f7e3ca27388bfdcec31d954a448a0de7dbc85078
Khushal vegad <khushalvegad11@gmail.com>
2023-09-20 18:33:38 +0530

    Day4

---
+++ Day-4.py
@@ -0,0 +1,43 @@
+# Random Module
+
+import random
+
+print(random.randint(1, 10))
+
+print(random.random())
+
+
+# List
+
+list1 = [1, 2, 3, 4, 5]
+
+print(list1)
+
+print(list1[0])
+
+print(list1[-1])
+
+print(len(list1))
+
+list1.append(6)
+
+print(list1)
+
+list1.insert(0, 0)
+
+print(list1)
+
+del list1[0]
+
+print(list1)
+
+list1.remove(2)
+
+print(list1)
+
+list1.reverse()
+
+print(list1)
+
+
+
